1e45664e9bccdd74a8f72baf85d264d4f5dfa4ad
Wolke Letzet <wolke.letzet@gmail.com>
2022-02-10 15:34:18 -0300

    ready

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,11 +1,8 @@
 <tr>
     <td class="header">
         <a href="{{ $url }}" style="display: inline-block;">
-            @if (trim($slot) === 'Laravel')
-                <img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
-            @else
-            <img src="https://ligup-v2.s3-sa-east-1.amazonaws.com/quintero/media/quinterologo-01.png" alt="Laravel Logo" style="height: 100%; width: 100% ; max-height: 300px">
-            @endif
+            <img src="https://ligup-v2.s3-sa-east-1.amazonaws.com/quintero/media/quinterologo-01.png" alt="Logo muni" style="height: 100%; width: 100% ; max-height: 300px">
+           
         </a>
     </td>
 </tr>
